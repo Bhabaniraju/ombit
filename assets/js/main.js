aff9a496a8e40fde499ec9a8c5d27882558c016e
bhabaniraju@gmail.com <bhabaniraju@gmail.comm>
2023-07-23 10:34:19 +0530

    brand change

--- assets/js/main.js
+++ assets/js/main.js
@@ -421,17 +421,45 @@
 })(jQuery); // End jQuery
 
 
-var root = document.getElementById('portrait');
 
-var mouse_monitor = function(e) {
-   let x = e.clientX/innerWidth;
-   let y = e.clientY/innerHeight;
-   
-   let move_x = (x>0.5) ? '-30px' : '30px';
-   let move_y = (y>0.5) ? '-20px' : '20px';
-   
-   root.style.setProperty("--translate-x", move_x);
-   root.style.setProperty("--translate-y", move_y);
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Brands", "Products", "Models"];
+const typingDelay = 90;
+const erasingDelay = 90;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
 }
 
-root.addEventListener("mousemove", mouse_monitor);
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
